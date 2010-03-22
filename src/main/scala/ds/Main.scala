3a475a80d0bdd132269d90ddc5138d4e69f59096
Nick Partridge <nkpart@gmail.com>
2010-03-23 09:43:31 +1100

    As we ended up.

--- src/main/scala/ds/Main.scala
+++ src/main/scala/ds/Main.scala
@@ -1,63 +1,75 @@
 package ds
 
 trait FoldLeft[F[_]] {
-  def foldLeft[B, A](t: F[A], b: B, f: (B, A) => B): B
+  def foldLeft[A, B](xs: F[A], b: B, f: (B, A) => B): B
 }
 
 object FoldLeft {
-  implicit object ListFoldLeft extends FoldLeft[List] {
-    def foldLeft[B, A](t: List[A], b: B, f: (B, A) => B): B = t.foldLeft(b)(f)
-  }
-  
-  implicit object OptionFoldLeft extends FoldLeft[Option] {
-    def foldLeft[B, A](t: Option[A], b: B, f: (B, A) => B): B = t match {
-      case Some(x) => f(b, x)
-      case None => b
-    }
+  implicit object FoldLeftList extends FoldLeft[List] {
+    def foldLeft[A, B](xs: List[A], b: B, f: (B, A) => B): B = xs.foldLeft(b)(f)
   }
 }
 
-trait Monoid[T] {
-  def mappend(a: T, b: T): T
-  def mzero: T
+trait Monoid[A] {
+  def mappend(a1: A, a2: A): A
+  def mzero: A   
 }
 
 object Monoid {
-  implicit object IntMonoid extends Monoid[Int] {
+  implicit val IntMonoid: Monoid[Int] = new Monoid[Int] {
     def mappend(a: Int, b: Int): Int = a + b
     def mzero: Int = 0
   }
-
+  
   implicit object StringMonoid extends Monoid[String] {
     def mappend(a: String, b: String): String = a + b
     def mzero: String = ""
   }
 }
 
-object Main {
-  implicit def toIdent[A](a: A) = new Identity[A] { val value = a }
+trait Identity[A] {
+  val value: A
   
-  def sum[T, M[_]](xs: M[T])(implicit m: Monoid[T], f: FoldLeft[M]): T = f.foldLeft(xs, m.mzero, m.mappend)
+  def |+|(a2: A)(implicit m: Monoid[A]): A = m.mappend(value, a2)
   
-//  def sum[T](xs: List[T]): T = ...
-//  def sum[M[_], T](xs: M[T]): T = ...
+  
+}
 
-  def plus[T](a: T, b: T)(implicit m: Monoid[T]): T = m.mappend(a, b)
+trait MA[M[_], A] {
+  val value: M[A]
   
+  def sum(implicit m: Monoid[A], fl: FoldLeft[M]): A =
+    fl.foldLeft(value, m.mzero, m.mappend)
+    
+  
+}
+
+object Main {
+  implicit def toIdentity[A](a: A): Identity[A] = new Identity[A] {
+    val value = a
+  }
+  
+  implicit def toMA[M[_], A](ma: M[A]): MA[M,A] = new MA[M,A] {
+    val value = ma
+  }
+  
+  val multMonoid = new Monoid[Int] {
+    def mappend(a: Int, b: Int): Int = a * b
+    def mzero: Int = 1
+  }
   
   def p(a: Any) { println("###> " + a) }
   def main(args: Array[String]) {
     println
     
-    p(3.plus(4))
-    
+    p(3 |+| 4)
     p(List(1,2,3).sum)
     
-                //     
-                // p(sum(List(1,2,3,4)))
-                // p(sum(List("a","b")))
-                // p(sum(Some(5): Option[Int]))
-                // p(sum(None: Option[Int]))
+    
+    // p(sum(List(1,2,3,4)))
+    //     p(sum(List("a", "b", "c")))
+    //     p(sum(List(1,2,3,4))(multMonoid, implicitly[FoldLeft[List]]))
+    //     
     println
   }
 }
